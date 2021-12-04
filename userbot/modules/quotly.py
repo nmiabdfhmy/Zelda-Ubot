3017eeb59ce202967d78ed9cdadb2eac583bb4e0
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-12-03 16:49:32 -0800

    update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -14,6 +14,44 @@ from userbot.utils import edit_delete, edit_or_reply
 
 
 @bot.on(zelda_cmd(outgoing=True, pattern=r"q ?(.*)"))
+async def quotess(qotli):
+    if qotli.fwd_from:
+        return
+    if not qotli.reply_to_msg_id:
+        return await qotli.edit("**Mohon Balas Ke Pesan**")
+    reply_message = await qotli.get_reply_message()
+    if not reply_message.text:
+        return await qotli.edit("**Mohon Balas Ke Pesan**")
+    chat = "@QuotLyBot"
+    if reply_message.sender.bot:
+        return await qotli.edit("**Mohon Balas Ke Pesan**")
+    await qotli.edit("**Sedang Memproses Sticker, Mohon Menunggu**")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(
+                        incoming=True,
+                        from_users=1031952739))
+                msg = await bot.forward_messages(chat, reply_message)
+                response = await response
+                """ - don't spam notif - """
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                return await qotli.reply("**Harap Jangan Blockir @QuotLyBot Buka Blokir Lalu Coba Lagi**")
+            if response.text.startswith("Hi!"):
+                await qotli.reply("**Silahkan Unblock @QuotLyBot dan coba lagi!!**")
+            else:
+                await qotli.delete()
+                await bot.forward_messages(qotli.chat_id, response.message)
+                await bot.send_read_acknowledge(qotli.chat_id)
+                """ - cleanup chat after completed - """
+                await qotli.client.delete_messages(conv.chat_id, [msg.id, response.id])                      
+    except TimeoutError:
+        await qotli.edit()
+
+
+@bot.on(zelda_cmd(outgoing=True, pattern=r"qq ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -29,7 +67,7 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1031952739)
             )
-            # first = await conv.send_message(f"/qcolor {warna}")
+            first = await conv.send_message(f"/qcolor {warna}")
             ok = await conv.get_response()
             await asyncio.sleep(2)
             second = await bot.forward_messages(chat, reply_message)
@@ -44,13 +82,13 @@ async def _(event):
         else:
             await event.delete()
             await bot.forward_messages(event.chat_id, response.message)
-    await bot.delete_messages(conv.chat_id, [ok.id, second.id, response.id])
+    await bot.delete_messages(conv.chat_id, [first.id, ok.id, second.id, response.id])
 
 
 CMD_HELP.update(
     {
         "quotly": f"**Plugin : **`quotly`\
-        \n\n  •  **Syntax :** `{cmd}q` <warna>\
+        \n\n  •  **Syntax :** `{cmd}qq` <warna>\
         \n  •  **Function : **Membuat pesan mu menjadi sticker bisa custom warna background.\
     "
     }
