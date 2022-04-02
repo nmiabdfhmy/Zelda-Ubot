68f69d575f993d0c4bf2873977960e1ee68fadb0
Zelda Projects <56251778+nmiabdfhmy@users.noreply.github.com>
2022-04-02 19:21:29 +0700

    Add files via upload

---
+++ userbot/modules/hpx.py
@@ -0,0 +1,47 @@
+import asyncio
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP, StartTime, bot
+from userbot.utils import bash, edit_or_reply, zelda_cmd
+
+hpx_thumb = "https://telegra.ph/file/6c06e1d0a0183d0f4da06.jpg"
+
+@zelda_cmd(pattern="hpx(.*)")
+async def amireallycuan(cuan):
+    user = await bot.get_me()
+    reply_message = await cuan.get_reply_message()
+    capt = str(cuan.pattern_match.group(2))
+    link = str(cuan.pattern_match.group(1))
+    capti = capt.replace(".", " ")
+    thumb = hpx_thumb
+    output = (
+        f"{**capt**}\n\n"
+        f"⬇️ **KLIK UNTUK MENONTON** ⬇️\n"
+        f"{link}\n\n"
+        f"**TERVERIFIKASI BERKUALITAS💯**\n"
+        f"-----------------------------------\n"
+        f"📍**LIHAT LEBIH BANYAK :**\n@asupanhypersex\n@hyperseexx"
+    )
+    if thumb:
+        try:
+            logo = thumb
+            await cuan.delete()
+            msg = await bot.send_file(cuan.chat_id, logo, caption=output)
+            await asyncio.sleep(300)
+        except BaseException:
+            await cuan.edit(
+                output + "\n\n ***Tidak Ada Thumbnail!"
+                "\nHarap balas ke gambar untuk dijadikan thumbnail Content.**"
+            )
+    else:
+        await edit_or_reply(cuan, output)
+        
+        
+CMD_HELP.update(
+    {
+        "content_hpx": f"**Plugin : **`Content CH`\
+        \n\n  •  **Syntax :** `{cmd}hpx` <Link> <Caption>\
+        \n  •  **Function : **Untuk membuat Content Pada Channel.\
+    "
+    }
+)
